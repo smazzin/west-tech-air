25b7b6e128c7c7f0dc16696645b1975d636d5d42
Josh Hausotter <jhausotter@gmail.com>
2022-05-14 07:57:02 -0700

    style homepage hero

--- slices/HomeHero/index.vue
+++ slices/HomeHero/index.vue
@@ -1,10 +1,14 @@
 <template>
   <section class="home-hero">
-    <prismic-rich-text :field="slice.primary.title" class="title" />
-    <prismic-rich-text :field="slice.primary.description" />
-    <prismic-link :field="slice.primary.buttonLink">
-      {{ slice.primary.buttonText }}
-    </prismic-link>
+    <div class="container">
+      <div class="home-hero-content">
+        <prismic-rich-text :field="slice.primary.title" class="title" />
+        <prismic-rich-text :field="slice.primary.description" />
+        <prismic-link class="btn" :field="slice.primary.buttonLink">
+          {{ slice.primary.buttonText }}
+        </prismic-link>
+      </div>
+    </div>
   </section>
 </template>
 

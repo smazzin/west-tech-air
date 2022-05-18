67d8c15e78b35f577bbc3cddd9039e2ff2550fd4
Josh Hausotter <jhausotter@gmail.com>
2022-05-18 10:58:53 -0700

    style footer

--- components/FooterPrismic.vue
+++ components/FooterPrismic.vue
@@ -1,22 +1,26 @@
  <template>
-  <footer class="container grid-3 footer pb-5">
-    <p v-if="$store.state.footer === 'Please create a footer document'" class="logo">{{ $store.state.footer }}</p>
-		<div>
-			<prismic-rich-text :field="$store.state.footer.ServiceAreasTitle" />
-			<prismic-rich-text :field="$store.state.footer.ServiceAreas" />
-		</div>
-		<div class="logo">
-			<nuxt-link to="/" class="logo">
-				<prismic-image :field="$store.state.footer.logo" />
-			</nuxt-link>
-		</div>
-		<div>
-			<prismic-rich-text :field="$store.state.footer.ScheduleServiceTitle" />
-			<prismic-rich-text :field="$store.state.footer.phone" />
-			<prismic-rich-text :field="$store.state.footer.BusinessHours" />
-		</div>
-		<div class="copyright">
-			© {{new Date().getFullYear()}} West Tech Air. All rights reserved.
+  <footer class="bg-black-base text-white-base">
+		<div class="container">
+			<p v-if="$store.state.footer === 'Please create a footer document'" class="logo">{{ $store.state.footer }}</p>
+			<div class="grid-3 footer">
+				<div>
+					<prismic-rich-text :field="$store.state.footer.ServiceAreasTitle" />
+					<prismic-rich-text :field="$store.state.footer.ServiceAreas" />
+				</div>
+				<div class="logo">
+					<nuxt-link to="/" class="logo">
+						<prismic-image :field="$store.state.footer.logo" />
+					</nuxt-link>
+				</div>
+				<div>
+					<prismic-rich-text :field="$store.state.footer.ScheduleServiceTitle" />
+					<prismic-rich-text :field="$store.state.footer.phone" />
+					<prismic-rich-text :field="$store.state.footer.BusinessHours" />
+				</div>
+			</div>
+			<div class="copyright text-center">
+				<span>© {{new Date().getFullYear()}} West Tech Air. All rights reserved.</span>
+			</div>
 		</div>
   </footer>
 </template>
